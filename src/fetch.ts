ef762eae0647158bfed93aa4563645798f5a3a03
bapplebo <bapplebo@gmail.com>
2025-01-08 22:01:06 -0800

    Update endpoint to fetch costs

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchLua = async () => {
   const response = await axios.get(
-    'https://raw.githubusercontent.com/DrunkFlamingo/DrunkFlamingo_TotalWarMods/master/warhammer_3/df_tabletopcaps/script/ttc/ttc_vanilla_units.lua'
+    'https://raw.githubusercontent.com/chadvandy/groovy_ttc/refs/heads/master/script/ttc/ttc_vanilla_units.lua'
   );
   return response.data;
 };
